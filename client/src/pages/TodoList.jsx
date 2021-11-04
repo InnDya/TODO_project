9324d780bdd3d9b0d1719f57fda4e343c1864245
Inna Dyachkova <inna.dyachkova@gmail.com>
2021-11-04 13:21:05 +0100

    Another way to edit task in todo list

--- client/src/pages/TodoList.jsx
+++ client/src/pages/TodoList.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 const moment = require('moment');
 
-export default function TodoList({list}) {
-    const [editMode, setEditMode] = useState(false);
+export default function TodoList({ list }) {
 
-    const enableEditMode = (e) => {
+    const editTask = (e) => {
         e.preventDefault();
-        setEditMode(true);
-        console.log("edit mode activated");
-    }
-
-    const disableEditMode = (e) => {
-        e.preventDefault();
-        setEditMode(false);
-        console.log("edit mode deactivated");
+        console.log(e.target.defaultValue);
     }
 
     return (
         <div id="card" className="card mt-5 mx-5 border border-primary">
             <div className="card-body">
                 <h5 className="card-title text-center text-primary">{list.title}</h5>
-                    <ul className="list-group mb-4">
+                <ul className="list-group mb-4">
                     {list.tasks.map((task) =>
-                        <li className="list-group-item" key={task._id}>
-                            <input className="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
-                            <div onDoubleClick={(e) => enableEditMode(e)}>
-                                {editMode ? <input type="text" className="textInput" value={`${task.task}`} onBlur={(e) => disableEditMode(e) } /> : task.task }
+                        <li className="list-group list-group-flush" key={task._id}>
+                            <div className="input-group mb-3">
+                                <div className="input-group-text">
+                                    <input className="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
+                                </div>
+                                <input type="text" 
+                                    className="form-control" 
+                                    aria-label="Text input with checkbox" 
+                                    defaultValue={task.task}
+                                    onClick={(e)=> editTask(e)}
+                                />
                             </div>
                         </li>
                     )}
-                    </ul>
+                </ul>
                 {moment(list.last_modified).fromNow()}
             </div>
         </div>
